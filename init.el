0d95ba1c610f4eaa2199f6a760ab8a7ece0a1b75
Dimitri Lozeve <dimitri@lozeve.com>
2020-03-28 11:41:34 +0100

    Add org-roam configuration
    https://org-roam.readthedocs.io/en/master/

--- init.el
+++ init.el
@@ -539,6 +539,18 @@
 
   (setq bibtex-autokey-year-length 4))
 
+(use-package org-roam
+  :ensure t
+  :hook (after-init . org-roam-mode)
+  :custom (org-roam-directory "~/notes/notes")
+  :bind (:map org-roam-mode-map
+              (("C-c n l" . org-roam)
+               ("C-c n f" . org-roam-find-file)
+               ("C-c n b" . org-roam-switch-to-buffer)
+               ("C-c n g" . org-roam-show-graph))
+              :map org-mode-map
+              (("C-c n i" . org-roam-insert))))
+
 (use-package ox-gfm
   :ensure t
   :after (org)
