2414165a65a9fc84674ac388c6d8d3e56b495e23
Dimitri Lozeve <dimitri@lozeve.com>
2020-05-05 12:19:23 +0200

    Update config: org-roam and disabled commands

--- init.el
+++ init.el
@@ -30,6 +30,9 @@
 
 (setq auto-window-vscroll nil)
 
+;; Enable disabled commands
+(put 'narrow-to-region 'disabled nil)
+
 ;; more useful frame title, that show either a file or a
 ;; buffer name (if the buffer isn't visiting a file)
 (setq frame-title-format
@@ -550,7 +553,7 @@
               (("C-c n l" . org-roam)
                ("C-c n f" . org-roam-find-file)
                ("C-c n b" . org-roam-switch-to-buffer)
-               ("C-c n g" . org-roam-graph-show))
+               ("C-c n g" . org-roam-graph))
               :map org-mode-map
               (("C-c n i" . org-roam-insert)))
   :config
