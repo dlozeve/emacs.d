9298c16c779fb23a0069262208eb7a63a704c9b7
Dimitri Lozeve <dimitri@lozeve.com>
2020-06-02 16:38:09 +0200

    Use clangd instead of ccls

--- init.el
+++ init.el
@@ -261,15 +261,6 @@
   ;; add company-lsp as a backend
   :config (push 'company-lsp company-backends))
 
-(use-package ccls
-  :ensure t
-  :config
-  (setq ccls-executable "ccls")
-  (setq lsp-prefer-flymake nil)
-  (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
-  :hook ((c-mode c++-mode objc-mode) .
-         (lambda () (require 'ccls) (lsp))))
-
 (use-package python-docstring
   :ensure t)
 
