ff4ded6f14071a85b8811bb412826d33480ee598
Dimitri Lozeve <dimitri@lozeve.com>
2021-07-22 21:03:33 +0200

    Update to org-roam v2

--- init.el
+++ init.el
@@ -697,25 +697,22 @@
 (use-package org-roam
   :straight t
   :after (org)
-  :hook (after-init . org-roam-mode)
   :custom
-  (org-roam-directory "~/notes/notes")
-  :bind
-  (:map org-roam-mode-map
-        (("C-c n l" . org-roam)
-         ("C-c n f" . org-roam-find-file)
-         ("C-c n b" . org-roam-switch-to-buffer)
-         ("C-c n g" . org-roam-graph)
+  (org-roam-directory (file-truename "~/notes/notes"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+	 ("C-c n f" . org-roam-node-find)
+	 ("C-c n g" . org-roam-graph)
+	 ("C-c n i" . org-roam-node-insert)
+	 ("C-c n c" . org-roam-capture)
+	 ;; Dailies
 	 ("C-c n j" . org-roam-dailies-capture-today)
-	 ("C-c n t" . org-roam-dailies-find-today)))
-  (:map org-mode-map
-	(("C-c n i" . org-roam-insert)))
+	 ("C-c n t" . org-roam-dailies-find-today))
   :config
+  (org-roam-setup)
+  (setq org-roam-v2-ack t)
   (setq org-roam-graph-executable "dot")
   (setq org-roam-graph-extra-config '(("overlap" . "false") ("rankdir" . "LR")))
   (setq org-roam-completion-system 'default)
-  (setq org-roam-link-use-custom-faces 'everywhere)
-  (set-face-attribute 'org-roam-link nil :foreground "lime green")
   (require 'org-roam-protocol)
   (setq org-roam-dailies-directory "daily/"))
 
