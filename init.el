2f66c7d2133516173f50cf263b260680607b9c6a
Dimitri Lozeve <dimitri@lozeve.com>
2021-07-28 18:26:50 +0200

    Add embark

--- init.el
+++ init.el
@@ -171,6 +171,32 @@
   :init
   (marginalia-mode +1))
 
+(use-package embark
+  :straight t
+  :bind
+  (("C-;" . embark-act)         ;; pick some comfortable binding
+   ("M-;" . embark-dwim)
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+  :init
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+  :config
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :straight t
+  :after (embark consult)
+  :demand t ; only necessary if you have the hook below
+  ;; if you want to have consult previews as you move around an
+  ;; auto-updating embark collect buffer
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
 (use-package deadgrep
   :straight t
   :bind (("<f5>" . deadgrep)))
