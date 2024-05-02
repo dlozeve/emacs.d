85dae4bedd2e9763258fcea0323162e6312f8ed0
Dimitri Lozeve <dimitri@lozeve.com>
2024-05-02 10:48:21 +0200

    Remove activities.el

--- init.el
+++ init.el
@@ -237,25 +237,6 @@
   :config
   (setq vundo-glyph-alist vundo-unicode-symbols))
 
-(use-package activities
-  :straight t
-  :init
-  (activities-mode)
-  (activities-tabs-mode)
-  ;; Prevent `edebug' default bindings from interfering.
-  (setq edebug-inhibit-emacs-lisp-mode-bindings t)
-  :bind
-  (("C-x C-a C-n" . activities-new)
-   ;; As resuming is expected to be one of the most commonly used
-   ;; commands, this binding is one of the easiest to press.
-   ("C-x C-a C-a" . activities-resume)
-   ("C-x C-a C-s" . activities-suspend)
-   ("C-x C-a C-k" . activities-kill)
-   ;; This binding mirrors, e.g. "C-x t RET".
-   ("C-x C-a RET" . activities-switch)
-   ("C-x C-a g" . activities-revert)
-   ("C-x C-a l" . activities-list)))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Knowledge management: org-mode, org-roam, bibliography
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
