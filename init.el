dedb88a23d9b503e06f8f54e8333ed1dd376b1d4
Dimitri Lozeve <dimitri@lozeve.com>
2026-03-23 08:49:42 +0100

    Remove agent-shell

--- init.el
+++ init.el
@@ -979,15 +979,6 @@
 (use-package gptel
   :ensure t)
 
-(use-package acp
-  :ensure (:host github :repo "xenodium/acp.el" :branch "main"))
-
-(use-package agent-shell
-  :ensure (:host github :repo "xenodium/agent-shell" :branch "main")
-  :custom
-  (agent-shell-preferred-agent-config (agent-shell-anthropic-make-claude-code-config))
-  (agent-shell-session-strategy 'new))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; External media
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
